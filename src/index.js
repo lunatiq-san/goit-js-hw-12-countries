2a99c9b7acd53caa0db29c4a4fcc7bda06a3dcb7
Rozumiak Vladyslav <lunatiq86@gmail.com>
2021-05-24 15:49:38 +0300

    Finish task

--- src/index.js
+++ src/index.js
@@ -1,54 +1,49 @@
-// 4. Если бекенд вернул больше чем 10 стран подошедших под критерий введенный пользователем, в интерфейсе отображается нотификация о том, что необходимо сделать запрос более специфичным. Для оповещений используй плагин pnotify.
-
 import './sass/main.scss';
 import countryCardTpl from './templates/country.hbs';
 import debounce from 'lodash.debounce';
 import { alert } from '@pnotify/core';
 import '@pnotify/core/dist/BrightTheme.css';
+import API from './js/fetchCountries';
+import getRefs from './js/get-refs';
 
-const refs = {
-  input: document.querySelector('.js-input'),
-  list: document.querySelector('.js-list'),
-};
+const refs = getRefs();
 
-// 2. HTTP запрос на бекенд при наборе имени страны в инпуте, событие input
-// 3. Обработчик события через debounce 500ms (lodash.debounce)
 refs.input.addEventListener('input', debounce(onInputChange, 500));
 
-// 1. Поиск данных о стране через Rest Countries API, /name, возвращающий массив обьектов стран попавших под критерий поиска
 function onInputChange(event) {
-  const inputValue = event.target.value;
+  const searchQuery = event.target.value;
 
-  fetch(`https://restcountries.eu/rest/v2/name/${inputValue}`)
-    .then(response => {
-      return response.json();
-    })
-    .then(countries => {
-      if (countries.length > 10) {
-        refs.list.innerHTML = '';
-        alert({
-          text: 'Too many matches found. Please enter a more specific query!',
-          delay: 2000,
-          mouseReset: true,
-          sticker: false,
-        });
-      }
-      // 5. Если бекенд вернул от 2-х до 10-х стран, под инпутом отображается список имен найденных стран.
-      if ((countries.length > 1) & (countries.length < 11)) {
-        refs.list.innerHTML = createListCountriesMarkup(countries);
-      }
+  API.fetchCountries(searchQuery).then(renderCountryMarkup).catch(onFetchError);
+}
 
-      // 6. Если бекенд вернул массив с одной страной, в интерфейсе рендерится разметка с данными о стране: название, столица, население, языки и флаг.
-      if (countries.length === 1) {
-        refs.list.innerHTML = '';
-        refs.list.insertAdjacentHTML('afterbegin', countryCardTpl(countries[0]));
-      }
-    })
-    .catch(onFetchError);
+function renderCountryMarkup(countries) {
+  clearMarkup();
+
+  if (countries.length === 1) {
+    refs.list.insertAdjacentHTML('afterbegin', countryCardTpl(countries[0]));
+  }
+
+  if ((countries.length > 1) & (countries.length < 11)) {
+    createListCountriesMarkup(countries);
+  }
+
+  if (countries.length > 10) {
+    alert({
+      text: 'Too many matches found. Please enter a more specific query!',
+      delay: 2000,
+      mouseReset: true,
+      sticker: false,
+    });
+  }
+}
+
+function clearMarkup() {
+  refs.list.innerHTML = '';
 }
 
 function createListCountriesMarkup(countries) {
-  return countries.map(country => `<li>${country.name}</li>`).join('');
+  const countriesList = countries.map(country => `<li>${country.name}</li>`).join('');
+  return (refs.list.innerHTML = countriesList);
 }
 
 function onFetchError(error) {
---
+++ src/js/get-refs.js
@@ -0,0 +1,6 @@
+export default function getRefs() {
+  return {
+    input: document.querySelector('.js-input'),
+    list: document.querySelector('.js-list'),
+  };
+}
